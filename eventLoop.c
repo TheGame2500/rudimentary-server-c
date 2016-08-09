7ec81b9f50cc3f33e437c96199deaf25b99968d9
Bira Gabriel <gabriel.bira95@gmail.com>
2016-08-09 14:27:26 +0300

    Lazy parsing the path from the request header

--- eventLoop.c
+++ eventLoop.c
@@ -2,6 +2,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
+#include <stdlib.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,6 +12,8 @@
 #include "eventLoop.h"
 
 #define MAX_QUEUE 200
+//prototypes
+char* getPath(int socketDescriptor); 
 //Things to note :
 //1)Need to translate ints to network/host byte-order
 //2)Important structs:
@@ -29,10 +32,8 @@ void sig_handler(int signo) {
 }
 int listenToPort(int port) {
 	int socketDescriptor;
-	struct sockaddr_in sourceAddr;
 	struct sockaddr_in destAddr;
 	int errorCode;
-	int returnVal;
 
 	//server settings
 	memset(&destAddr,0, sizeof destAddr);
@@ -54,25 +55,21 @@ int listenToPort(int port) {
 			printf("Socket opened on port %i\n",port);
 		} else {
 			printf("Listen failed\n");
-			goto throwError;
+			goto exitGracefully;
 		}
 	} else {
-		printf("Bind failed");
-		goto throwError;
+		printf("Bind failed\n");
+		goto exitGracefully;
 	}
 
 //3. Handle responses by sending back HTML;
 	while(1) {
-		struct sockaddr cliAddrRaw;
 		printf("Accepting..\n");
 		int clientDescriptor = accept(socketDescriptor, NULL, NULL);
 		if(clientDescriptor >= 0){
-			char buffer[1024];
-			printf("Client descriptor %i",clientDescriptor);
-			read(clientDescriptor, &buffer, sizeof buffer);
-			int i;
-			for(i=0;i<sizeof buffer;i++)
-				printf("%c",buffer[i]);
+			//simple lazy parsing of the request header, just get the first line and extract file path :)
+			char* filePath = getPath(clientDescriptor);
+			printf("filePath : %s \n", filePath);
 			close(clientDescriptor);
 		}
 		else {
@@ -81,15 +78,41 @@ int listenToPort(int port) {
 
 		if(signal(SIGKILL, sig_handler) != SIG_ERR){
 			//exit gracefully
-			goto throwError;
+			goto exitGracefully;
 		}
 		
 	}
 	
-throwError:
-	printf("ERROR : %i\n", errorCode);
+exitGracefully:
+	if(errorCode != 0)
+		printf("ERROR : %i\n", errorCode);
 	//close socket if it exists
 	if(socketDescriptor != -1)
 		close(socketDescriptor);
 	return errorCode;
+}
+
+char* getPath(int socketDescriptor) {
+	//get the whole request header into a buffer so we can have fun with it later
+	char* request = (char *)malloc(1);
+	char buffer[1024];
+	int start = read(socketDescriptor, &buffer, sizeof(buffer)); 
+	while( start != 0 && start != -1 ) {
+		request = (char *) realloc(request, sizeof(request)+sizeof(buffer));
+		request = strcat(request, buffer);
+		if(strstr(buffer, "\r\n\r\n") != NULL) break;
+		start = read(socketDescriptor, &buffer, sizeof(buffer)); 
+	}
+
+	//fun time
+	//skip first 4 chars "GET " and then get everything until " "
+	int counter = 4;
+	static char path[50] = ""; 
+
+	while(request[counter] != ' ') {
+		path[ strlen(path) ] = request[counter];
+		counter++;
+	}
+
+	return path;
 }
